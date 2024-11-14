a0f437339ce9e69179d566cd1115bc94b130d6ac
med4h <adrian.meda6@gmail.com>
2024-11-14 10:56:33 -0800

    added express route handlers

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
 //TODO: Create your GET Request Route Below: 
 app.get("/restaurants", async (req, res) => {
     const restaurants = await Restaurant.findAll({});
@@ -14,6 +17,21 @@ app.get("/restaurants/:id", async (req, res) => {
     const restaurantsId = await Restaurant.findByPk(id);
     res.json(restaurantsId);
 });
+
+app.post("/restaurants", async (req,res) => {
+    const restaurant = await Restaurant.create(req.body);
+    res.json(restaurant);
+})
+
+app.put("/restaurants/:id", async (req, res) => {
+    const updatedRest = await Restaurant.update(req.body, {where: {id: req.params.id}});
+    res.json(updatedRest);
+})
+
+app.delete("restaurants/:id" , async (req, res) => {
+    const deleteRest = await Restaurant.destroy({where: {id: req.params.id}});
+    res.json(deleteRest);
+})
 
 
 module.exports = app;
